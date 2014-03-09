9fa2d4706a2cc75f94b554a6b45ee0a1de4edff1
RedFoxBot <redfox@nildram.co.uk>
2014-03-09 16:18:49 +0000

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -24,42 +24,51 @@ $html = oneline(scraperwiki::scrape("http://www.ukrlp.co.uk/ukrlp/ukrlp_provider
 
 preg_match_all('|<div class="pod_main_body">(.*?<div )class="searchleft">|',$html,$arr);
 
-echo json_encode($arr);
-
    if (isset($arr[1][0])) { $code = $arr[1][0];} else { $code='';}
         if ($code!='') {
+            echo "code = " + $code + ".<br/>"
 
-preg_match_all('|<div class="provhead">UKPRN: ([0-9]*?)</div>|',$code,$num);
- if (isset($num [1][0])) { $num  = trim($num [1][0]);} else { $num ='';}
-
-preg_match_all('|</div>.*?<div class="provhead">(.*?)<|',$code,$name);
-if (isset($name [1][0])) { $name = trim($name [1][0]);} else { $name ='';}
-
-preg_match_all('|<div class="tradingname">Trading Name: <span>(.*?)</span></div>|',$code,$trading);
-if (isset($trading[1][0])) { $trading = trim($trading[1][0]);} else { $trading='';}
-
-preg_match_all('|<div class="assoc">Legal address</div>(.*?)<div|',$code,$legal);
-if (isset($legal [1][0])) { $legal = trim($legal [1][0]);} else { $legal ='';}
-
-
-preg_match_all('|<div class="assoc">Primary contact address</div>(.*?)<div|',$code,$primary);
-if (isset($primary[1][0])) { $primary= trim($primary[1][0]);} else { $primary='';}
-
-$primary = parseAddress($primary);
-$legal= parseAddress($legal);
-
-        if (trim($name)!='') {
-echo "saving";
-scraperwiki::save(array('num'), array('num' => "".clean($num),'name' => clean($name),'trading' => clean($trading),
-                                             'legal_address' => clean($legal['address']),'legal_phone' => clean($legal['phone']),
-                                            'legal_fax' => clean($legal['fax']),'legal_email' => clean($legal['email']),
-                                        'legal_web' => clean($legal['web']),
-                                    'primary_address' => clean($primary['address']),'primary_phone' => clean($primary['phone']),
-                                            'primary_fax' => clean($primary['fax']),'primary_email' => clean($primary['email']),
-                                      'primary_web' => clean($primary['web']), 'primary_courses' => clean($primary['courses']) ));    
-    }
-      scraperwiki::save_metadata('counter',$counter);  
-    }
+            preg_match_all('|<div class="provhead">UKPRN: ([0-9]*?)</div>|',$code,$num);
+            if (isset($num [1][0])) { $num  = trim($num [1][0]);} else { $num ='';}
+             
+            echo "num = " + $num + ".<br/>"
+            
+            preg_match_all('|</div>.*?<div class="provhead">(.*?)<|',$code,$name);
+            if (isset($name [1][0])) { $name = trim($name [1][0]);} else { $name ='';}
+            
+            echo "name = " + $name + ".<br/>"
+            
+            preg_match_all('|<div class="tradingname">Trading Name: <span>(.*?)</span></div>|',$code,$trading);
+            if (isset($trading[1][0])) { $trading = trim($trading[1][0]);} else { $trading='';}
+            
+            echo "trading = " + $trading + ".<br/>"
+            
+            preg_match_all('|<div class="assoc">Legal address</div>(.*?)<div|',$code,$legal);
+            if (isset($legal [1][0])) { $legal = trim($legal [1][0]);} else { $legal ='';}
+            
+            echo "legal = " + $legal + ".<br/>"
+            
+            preg_match_all('|<div class="assoc">Primary contact address</div>(.*?)<div|',$code,$primary);
+            if (isset($primary[1][0])) { $primary= trim($primary[1][0]);} else { $primary='';}
+            
+            echo "primary = " + $primary + ".<br/>"
+            
+            $primary = parseAddress($primary);
+            $legal= parseAddress($legal);
+            
+            if (trim($name)!='') {
+                echo "saving";
+                blarg;
+                scraperwiki::save(array('num'), array('num' => "".clean($num),'name' => clean($name),'trading' => clean($trading),
+                                                             'legal_address' => clean($legal['address']),'legal_phone' => clean($legal['phone']),
+                                                            'legal_fax' => clean($legal['fax']),'legal_email' => clean($legal['email']),
+                                                        'legal_web' => clean($legal['web']),
+                                                    'primary_address' => clean($primary['address']),'primary_phone' => clean($primary['phone']),
+                                                            'primary_fax' => clean($primary['fax']),'primary_email' => clean($primary['email']),
+                                                      'primary_web' => clean($primary['web']), 'primary_courses' => clean($primary['courses']) ));    
+                }
+            scraperwiki::save_metadata('counter',$counter);  
+        }
     }
 
     function parseAddress($val) {
